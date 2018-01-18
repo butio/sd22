af78e28c585f07d164227f48b98eba5c2aa6b112
ohs60233 <ohs60233@win10ohs60233>
2018-01-18 17:04:35 +0900

    daaaa

--- src/MemberLogin.java
+++ src/MemberLogin.java
@@ -1,12 +1,16 @@
 
 
 import java.io.IOException;
+import java.sql.ResultSet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Dao.*;
 
 /**
  * Servlet implementation class MemberLogin
@@ -39,7 +43,18 @@ public class MemberLogin extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 
-		
+		 HttpSession session = request.getSession();
+		 
+		 String mail = request.getParameter("mail");
+		 String pass = request.getParameter("pass");
+		 
+		 Dao dao = null;
+		 ResultSet rs = null;
+		 try{
+			 dao = new Dao();
+			 rs = dao.execute("SELECT ")
+		 }
+		 
 	}
 
 }
