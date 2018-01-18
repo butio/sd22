52ae04a954bdfa2a88582d3c2a4c8cbdcfb1f10c
ohs60233 <ohs60233@win10ohs60233>
2018-01-18 13:42:17 +0900

    Dao

--- src/Dao/Dao.java
+++ src/Dao/Dao.java
@@ -1,5 +1,49 @@
 package Dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 public class Dao {
+	private final String DB_HOST = "localhost";
+	private final String DB_NAME ="sd22";
+	private final String DB_USER = "root";
+	private final String DB_PASS = "";
 
+	private Connection cn = null;
+	private Statement st = null;
+
+	public Dao()throws SQLException , ClassNotFoundException{
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			this.cn = DriverManager.getConnection("jdbc:mysql://"+ DB_HOST +"/ "+ DB_NAME +" ?useUnicode=true&characterEncoding=utf8", DB_USER,DB_PASS);
+			this.st = cn.createStatement();
+		}catch (SQLException e) {
+			throw e;
+		}catch (ClassNotFoundException e){
+			throw e;
+		}
+	}
+
+	public ResultSet execute(String sql)throws SQLException{
+		try{
+			return this.st.executeQuery(sql);
+		}catch(SQLException e) {
+			throw e;
+		}
+	}
+
+	public void close() throws SQLException{
+		try{
+			if(st != null){
+				st.close();
+			}if(cn != null){
+				cn.close();
+			}
+		}catch(SQLException e){
+			throw e;
+		}
+	}
 }
+
