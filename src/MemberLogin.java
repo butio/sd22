b4c5c56bd63ca6a106365d3eb3b926e395947e2c
ohs60233 <ohs60233@win10ohs60233>
2018-01-18 18:01:04 +0900

    buti

--- src/MemberLogin.java
+++ src/MemberLogin.java
@@ -2,7 +2,9 @@
 
 import java.io.IOException;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import Dao.*;
+import Dao.Dao;
 
 /**
  * Servlet implementation class MemberLogin
@@ -44,17 +46,37 @@ public class MemberLogin extends HttpServlet {
 		response.setCharacterEncoding("UTF-8");
 
 		 HttpSession session = request.getSession();
-		 
+
 		 String mail = request.getParameter("mail");
 		 String pass = request.getParameter("pass");
-		 
+
+		 ArrayList<ArrayList<String>> membertbl = new ArrayList<ArrayList<String>>();
 		 Dao dao = null;
 		 ResultSet rs = null;
 		 try{
 			 dao = new Dao();
-			 rs = dao.execute("SELECT ")
-		 }
-		 
-	}
+			 rs = dao.execute("SELECT * FROM member");
+			 if(mail.equals(rs.getString("member.user_mail")) && pass.equals(rs.getString("member.user_pass"))){
+				 session.setAttribute("user_mail", mail);
+				 session.setAttribute("user_pass", pass);
+				 session.setAttribute("user_nickname",rs.getString("member.user_nickname") );
+			 }else {
+				 String errormessage = "正しい情報を入力してください";
+			 }
+	}catch(Exception e){
 
+	}finally{
+		try{
+			if(rs != null){
+				rs.close();
+			}
+			dao.close();
+		}
+		catch(Exception e){
+
+		}
+	}
+		 RequestDispatcher rd = request.getRequestDispatcher("/logincomp.html");
+		 rd.forward(request, response);
+}
 }
